fcb6f316a3b8d2cc50311b0a5cfdbc1b56c24c6c
James <jameslfidlin@gmail.com>
2022-07-18 19:38:58 -0400

    added classes

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+const Manager = require('lib/Manager.js');
+const Engineer = require('lib/Engineer.js');
+const Intern = require('lib/Intern.js');
+
 let objects_array = [];
 
 function compile_objects(run_type){
-  let grammah, unique_input, unique_message = '';
+  let grammah, unique_input, unique_message, roll = '';
 
   if (run_type === "Start"){
+    roll = "Manager";
     grammah = "your"
-    unique_input = "office_number";
+    unique_input = "Office_Number";
   }else{
     grammah = "their";
-    if(run_type === "Add an Engineer") unique_input = "gitHub_username";
-    if(run_type === "Add an Intern") unique_input = "school";
+    if(run_type === "Add an Engineer") {
+      roll = "Engineer";
+      unique_input = "GitHub_Username";
+    }
+    if(run_type === "Add an Intern"){
+      roll = "Intern";
+      unique_input = "School";
+    }
   }
   
   unique_message = `Enter ${grammah} ${unique_input.replace("_", " ")}`;
@@ -59,117 +70,3 @@ function compile_objects(run_type){
 
   }
     compile_objects("Start");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-function compile_objects(run_type, data){
-  if (run_type === "Start"){
-    grammah = "your";
-  }else{
-    grammah = "their";
-  }
-
-  inquirer
-    .prompt([
-      {
-        type: 'input',
-        name: 'name',
-        message: `What is ${grammah} name?`,
-      },
-      {
-        type: 'input',
-        name: 'employee ID',
-        message: `Please enter ${grammah} Employee ID.`,
-      },
-      {
-        type: 'input',
-        name: 'email_address',
-        message: `Enter ${grammah} email address.`,
-      },
-      {
-        type: 'input',
-        name: 'office_number',
-        message: 'Enter your office number.',
-      },
-      {
-        type: 'input',
-        name: 'office_number',
-        message: 'Enter your office number.',
-      },
-      {
-        type: 'input',
-        name: 'office_number',
-        message: 'Enter your office number.',
-      },
-    ])
-    .then((data) => {
-      
-    })
-    .then((data) => {
-      inquirer
-      .prompt([{
-        type: 'list',
-        message: 'What would you like to do?',
-        name: 'type_of_employee',
-        choices: ['Add an Engineer', 'Add an Intern', 'Finish Building my Team'],
-      },
-    ])
-
-      fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
-        err ? console.log(err) : console.log('Success!')
-      );
-
-    })
-    
-    
-    
-
-
-}
-
-compile_objects(start);*/
-
-
-/*
-  function positionPrompt(data) {
-    inquirer.prompt({
-        type: "input",
-        name: "userInput",
-        message: "Please enter the ticker and amount, separated by a comma and space (AAPL, 30"
-    }).then(res => {
-        userInputArr = res.userInput.split(", ");
-        data.addToPosition(userInputArr[0], parseInt(userInputArr[1]));
-        console.log(data);
-        return data
-    }).then(data => {
-        inquirer.prompt({
-            type: "confirm",
-            name: "addAgain",
-            message: "Would you like to add again?"
-        }).then(userInput => {
-            if (userInput.addAgain) {
-                positionPrompt(data);
-            }
-        })
-    })
-}*/
