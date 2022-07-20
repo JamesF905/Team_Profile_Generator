391d6fa8ed90acee4fad41672f0f7a484cd933fb
James <jameslfidlin@gmail.com>
2022-07-20 02:50:14 -0400

    re-organised folders

--- assets/js/generateHTML.js
+++ assets/js/generateHTML.js
@@ -1,31 +1,5 @@
 const fs = require('fs');
 
-/*
-fs.readFile('src/index.html', 'utf8', (error, data) =>
-error ? console.error(error) : html_source(data)
-);
-
-
-function html_source(source){
-    console.log(source);
-    fs.writeFile('dist/index.html', source, (err) =>
-    err ? console.error(err) : console.log('Success!')
-    )
-}
-
-fs.mkdir('dist/assets/css', (err) =>
-    err ? console.error(err) : console.log('Success!')
-);
-*/
-
-/*
-fs.mkdir(css_path, { recursive: true }, function(){
-    fs.readFile('src/index.html', 'utf8', (error, data) => {
-        fs.writeFile(path, data, (err) =>
-        err ? console.error(err) : console.log('Success!'))
-    })
-});*/
-
 //Function that will read a fille from a source parameter, and create another file to send to the destination parameter
 function createFromSource(source, destination) {
     fs.readFile(source, 'utf8', function (err, data) {
