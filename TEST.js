4cd38189227b06a395f4fa06a076e625f40a1d2a
James <jameslfidlin@gmail.com>
2022-07-20 02:39:43 -0400

    editing for file creation

--- TEST.js
+++ TEST.js
@@ -17,8 +17,7 @@ fs.mkdir('dist/assets/css', (err) =>
     err ? console.error(err) : console.log('Success!')
 );
 */
-let path = 'dist';
-let css_path = `${path}/assets/css`;
+
 /*
 fs.mkdir(css_path, { recursive: true }, function(){
     fs.readFile('src/index.html', 'utf8', (error, data) => {
@@ -26,11 +25,26 @@ fs.mkdir(css_path, { recursive: true }, function(){
         err ? console.error(err) : console.log('Success!'))
     })
 });*/
-/*
-fs.readFile('src/index.html', 'utf8', (error, data) => {
-    fs.writeFile(path, data, (err) =>
-    err ? console.error(err) : console.log('Success!'))
-})*/
 
-fs.writeFile(path, data, (err) =>
-err ? console.error(err) : console.log('Success!'))
+//Function that will read a fille from a source parameter, and create another file to send to the destination parameter
+function createFromSource(source, destination) {
+    fs.readFile(source, 'utf8', function (err, data) {
+            if (err) console.error(err);
+            //Do your processing, MD5, send a satellite to the moon, etc.
+            fs.writeFile(destination, data, function(err) {
+                if (err) console.error(err);
+                console.log('Success!');
+            });
+        });
+}
+
+let path = 'dist'; // set destination folder for the generated files and folders
+let css_path = `${path}/assets/css`; // set destination folder for the css
+
+//Create all directories
+fs.mkdirSync(css_path, { recursive: true },  (err) =>
+    err ? console.error(err) : console.log('Success!')
+);
+
+createFromSource('src/index.html', `${path}/index.html`);
+createFromSource('src/assets/css/styles.css',`${css_path}/styles.css`);
