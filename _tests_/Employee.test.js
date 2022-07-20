3077a90f7b846911a17ccc3d638fa83369327cf2
James <jameslfidlin@gmail.com>
2022-07-20 18:34:47 -0400

    first tests finished - employee

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,29 +1,82 @@
 const Employee = require("../lib/Employee.js");
-describe("Algo", () => {
-  describe("reverse", () => {
-    it("should reverse a given string", () => {
-      // In order to test the function, we need to create a working example. First we define 'str'. 
-      const str = "Hello World!";
-      // Next we define 'reversed' as the value of 'str' reversed.
-      const reversed = "!dlroW olleH";
-      // We pass 'str' into '.reverse()' and set that value equal to result
-      const result = new Algo().reverse(str);
-      // We expect 'result' to equal 'reversed' if the 'reverse()' function is working correctly.
-      expect(result).toEqual(reversed);
+describe('Employee', () => {
+  describe("Make new Object", () => {
+    it('Should create an object with id, name, and email parameters as well as methods get(id), get(name), get(email)', () => {
+      const guy = new Employee('James', 1, 'james@gmail.com');
+      expect(guy.name).toEqual('James');
+      expect(guy.id).toEqual(1);
+      expect(guy.email).toEqual('james@gmail.com');
+    });
+
+    it ('Throw an error if missing any parameters', () => { 
+      const throwCheck = () => new Employee();
+
+      expect(throwCheck).toThrow();
+    });
+
+    it ('Throw an error if missing the id parameter', () => { 
+        const throwCheck = () => new Employee('James', 'james@gmail.com');
+
+        expect(throwCheck).toThrow();
+    });
+
+    it ('Throw an error if missing the name parameter', () => { 
+        const throwCheck = () => new Employee(1, 'james@gmail.com');
+
+        expect(throwCheck).toThrow();
+    });
+
+    it ('Throw an error if missing the email parameter', () => { 
+        const throwCheck = () => new Employee('James', 1);
+
+        expect(throwCheck).toThrow();
+    });
+
+    it ('Throw an error if "id" is an incorrect type', () => { // email wrong type
+      const throwCheck = () => new Employee('James', "1", 'james@gmail.com');
+
+      expect(throwCheck).toThrow();
+    });
+
+    it ('Throw an error if "name" is an incorrect type', () => { // email wrong type
+      const throwCheck = () => new Employee(1, 1, 'james@gmail.com');
+
+      expect(throwCheck).toThrow();
+    });
+
+    it ('Throw an error if "email" is an incorrect type', () => { // email wrong type
+      const throwCheck = () => new Employee('James', 1, false);
+
+      expect(throwCheck).toThrow();
+    });
   });
+
+  describe("getId", () => {
+    it ('Must return the "id" variable of the object', () => {
+      const james = new Employee('James', 1, 'james@gmail.com');
+      expect(james.getId()).toEqual(1);
+    });
+  });
+
+  describe("getName", () => {
+    it ('Must return the "Name" variable of the object', () => {
+      const james = new Employee('James', 1, 'james@gmail.com');
+      expect(james.getName()).toEqual('James');
+    });
+  });
+
+  describe("getEmail", () => {
+    it ('Must return the "Email" variable of the object', () => {
+      const james = new Employee('James', 1, 'james@gmail.com');
+      expect(james.getEmail()).toEqual('james@gmail.com');
+    });
+  });
+
+  describe("getRole", () => {
+    it ('Must return the "Role" variable of the object', () => {
+      const james = new Employee('James', 1, 'james@gmail.com');
+      expect(james.getRole()).toEqual('Employee');
+    });
+  });
+  
 });
-/*
-describe("Algo", () => {
-    describe("reverse", () => {
-      // 'reverse' should reverse "Hello World!"
-      it("should reverse a given string", () => {
-        // In order to test the function, we need to create a working example. First we define 'str'. 
-        const str = "Hello World!";
-        // Next we define 'reversed' as the value of 'str' reversed.
-        const reversed = "!dlroW olleH";
-        // We pass 'str' into '.reverse()' and set that value equal to result
-        const result = new Algo().reverse(str);
-        // We expect 'result' to equal 'reversed' if the 'reverse()' function is working correctly.
-        expect(result).toEqual(reversed);
-      });
-    });*/
